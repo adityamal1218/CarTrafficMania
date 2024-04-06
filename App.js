363f540953de6a89daf329ef881fdc538107e362
vikash kushwaha <vikashkumar9669901922@gmail.com>
2024-04-06 14:48:04 +0530

    setupcompleted

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from "react"
+import { StyleSheet, Text, View, Button, Link } from 'react-native';
+import { BarCodeScanner } from './node_modules/expo-barcode-scanner';
+
 
 export default function App() {
+  const [permission , setPermission] = useState(null)
+  const [scanned, setScanned] = useState(false);
+  const [text,setText] = useState("NOt Scanned")
+
+  const askForCamera=()=>{
+    (async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setPermission(status == 'granted')
+    })()
+  }
+
+  useEffect(() => {
+  askForCamera();
+
+  },[]);
+
+  const handleBarCode = ({type, data}) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: "+type+"\nData:"+data);
+  }
+  if(permission === null){
+    return(
+      <View style={styles.container}>
+      <Text>Allow TO access camera</Text>
+      </View>
+    )
+  }
+  if(permission === false){
+    return(
+      <View style={styles.container}>
+      <Text>Camera not found</Text>
+      <Button title='AlloW Camera' onPress={()=>askForCamera()}/>
+      </View>
+    )
+  }
+  
   return (
     <View style={styles.container}>
+      {/* <View style={styles.barcodebox}>
+        
+      </View> */}
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello vikash</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +61,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // barcodebox: {
+  //   backgroundColor: 'tomato',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   height: '320',
+  //   width: '320',
+  //   overflow: 'hidden',
+  //   borderRadius: '30',
+  // },
+  maintext:{
+    fontSize:'16',
+    margin: '20'
+  }
 });
